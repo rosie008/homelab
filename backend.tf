dd1d9fd3717c77340551e861d2dd2a5bb93d82be
rosie <riyadis008@gmail.com>
2026-06-06 22:03:58 +0700

    use newer format

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "rose-homelab-terraform-state"
-    key            = "homelab/terraform.tfstate"
-    region         = "ap-southeast-3"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "rose-homelab-terraform-state"
+    key          = "homelab/terraform.tfstate"
+    region       = "ap-southeast-3"
+    use_lockfile = true
+    encrypt      = true
   }
 }
